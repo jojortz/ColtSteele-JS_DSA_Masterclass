63a24e76386bd6db0eb0a7727bde8c975cd87719
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-01-04 22:07:43 -0800

    implemented two-pointer solution for sumZero.js

---
+++ test/sumZero.test.js
@@ -0,0 +1,29 @@
+//import { expect, jest, test } from '@jest/globals';
+const sumZero = require("../ProblemSolvingPatterns/MultiplePointers/sumZero");
+
+const examples = [
+  {
+    input: [-3, -2, -1, 0, 1, 2, 3],
+    expected: [-3, 3]
+  },
+  {
+    input: [-2, 0 , 1, 3],
+    expected: undefined
+  },
+  {
+    input: [1, 2, 3],
+    expected: undefined
+  },
+  {
+    input: [-4, -3, -2, -1, 0, 1, 2, 5, 6],
+    expected: [-2, 2]
+  },
+]
+
+describe("sumZero", () => {
+  examples.forEach((example) => {
+    test(`Input: ${JSON.stringify(example.input)}`, () => {
+      expect(sumZero(example.input)).toEqual(example.expected);
+    });
+  });
+});
