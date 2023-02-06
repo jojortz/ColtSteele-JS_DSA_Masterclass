59c68979be59d532a4539da77abea54f3bc854cf
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-02-06 14:40:02 -0800

    implement DFS with testing for BinarySearchTree

--- DataStructures/BinarySearchTree.js
+++ DataStructures/BinarySearchTree.js
@@ -58,6 +58,38 @@ class BinarySearchTree {
     }
     return result;
   }
+
+  DFS_PreOrder() {
+    let results = [];
+    function search(node) {
+      results.push(node.value);
+      if (node.left) search(node.left);
+      if (node.right) search(node.right);
+    };
+    search(this.root);
+    return results;
+  };
+  DFS_PostOrder() {
+    let results = [];
+    function search(node) {
+      if (node.left) search(node.left);
+      if (node.right) search(node.right);
+      results.push(node.value);
+    };
+    search(this.root);
+    return results;
+  };
+
+  DFS_InOrder() {
+    let results = [];
+    function search(node) {
+      if (node.left) search(node.left);
+      results.push(node.value);
+      if (node.right) search(node.right);
+    };
+    search(this.root);
+    return results;
+  }
 };
 
 module.exports = BinarySearchTree;
--- test/DataStructures/BinarySearchTree.test.js
+++ test/DataStructures/BinarySearchTree.test.js
@@ -1,6 +1,6 @@
 const BinarySearchTree = require("../../DataStructures/BinarySearchTree");
 
-let nodes = [2, 0, 1, 3]
+let nodes = [2, 0, 1, -1, 4, 3, 5]
 
 describe('BinarySearchTree', () => {
   let BST;
@@ -16,7 +16,7 @@ describe('BinarySearchTree', () => {
     expect(BST.root.value).toBe(2);
     expect(BST.root.left.value).toBe(0);
     expect(BST.root.left.right.value).toBe(1);
-    expect(BST.root.right.value).toBe(3);
+    expect(BST.root.right.value).toBe(4);
   })
   test('It should return undefined for repeated insert', () => {
     expect(BST.insert(1)).toBe(undefined);
@@ -28,14 +28,26 @@ describe('BinarySearchTree', () => {
       })
     })
     test('It should return false for nodes not in the tree', () => {
-      expect(BST.find(-1)).toBe(false);
+      expect(BST.find(-10)).toBe(false);
       expect(BST.find(10)).toBe(false);
     });
   })
 
   describe('Breadth First Search', ()=> {
     test('It should perform a BFS', () => {
-      expect(BST.BFS()).toEqual([2, 0, 3, 1]);
+      expect(BST.BFS()).toEqual([2, 0, 4, -1, 1, 3, 5]);
     })
   });
+  describe('Depth First Search', ()=> {
+    test('It should perform a DFS PreOrder', () => {
+      expect(BST.DFS_PreOrder()).toEqual([2, 0, -1, 1, 4, 3, 5]);
+    })
+    test('It should perform a DFS PostOrder', () => {
+      expect(BST.DFS_PostOrder()).toEqual([-1, 1, 0, 3, 5, 4, 2]);
+    })
+    test('It should perform a DFS InOrder', () => {
+      expect(BST.DFS_InOrder()).toEqual([-1, 0, 1, 2, 3, 4, 5]);
+    })
+  });
+
 })
