15b716d2648f78306e62dc4247c8c18c0a5faf36
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-02-06 16:16:06 -0800

    implement extractMax() in MaxBinaryHeap with testing

--- DataStructures/MaxBinaryHeap.js
+++ DataStructures/MaxBinaryHeap.js
@@ -15,8 +15,41 @@ class MaxBinaryHeap {
     return val;
   }
 
+  extractMax() {
+    if (this.values.length === 1) return this.values.pop();
+    let max = this.values[0];
+    let idx = 0;
+    this.values[idx] = this.values.pop();
+    while (true) {
+      let [childIdx1, childIdx2] = this.getChildren(idx);
+      if (childIdx1 > this.values.length - 1 && childIdx2 > this.values.length - 1) break;
+      if (this.values[childIdx1] > this.values[idx] ||
+        this.values[childIdx2] > this.values[idx]) {
+        if (this.values[childIdx1] > this.values[childIdx2]) {
+          this.swap(idx, childIdx1);
+          idx = childIdx1;
+        } else {
+          this.swap(idx, childIdx2);
+          idx = childIdx2;
+        }
+      } else {
+        break;
+      }
+    }
+    return max;
+  }
+
   getParent(n) {
     return Math.floor((n - 1) / 2);
+  }
+  getChildren(n) {
+    return [2*n + 1, 2*n + 2];
+  }
+
+  swap(idx1, idx2) {
+    let temp = this.values[idx1];
+    this.values[idx1] = this.values[idx2];
+    this.values[idx2] = temp;
   }
 }
 
--- test/DataStructures/MaxBinaryHeap.test.js
+++ test/DataStructures/MaxBinaryHeap.test.js
@@ -21,6 +21,27 @@ describe('MaxBinaryHeap', () => {
     expect(maxHeap.insert(-1)).toBe(-1);
     expect(maxHeap.values).toEqual([60, 55, 41, 27, 18, 33, 39, 12, -1]);
 
+  });
+
+  test('It should extract the max', () => {
+    maxHeap.insert(55);
+    maxHeap.insert(60);
+    expect(maxHeap.extractMax()).toBe(60);
+    expect(maxHeap.values).toEqual([55, 27, 41, 12, 18, 33, 39]);
+    expect(maxHeap.extractMax()).toBe(55);
+    expect(maxHeap.values).toEqual([41, 27, 39, 12, 18, 33]);
+    expect(maxHeap.extractMax()).toBe(41);
+    expect(maxHeap.values).toEqual([39, 27, 33, 12, 18]);
+    expect(maxHeap.extractMax()).toBe(39);
+    expect(maxHeap.values).toEqual([33, 27, 18, 12]);
+    expect(maxHeap.extractMax()).toBe(33);
+    expect(maxHeap.values).toEqual([27, 12, 18]);
+    expect(maxHeap.extractMax()).toBe(27);
+    expect(maxHeap.values).toEqual([18, 12]);
+    expect(maxHeap.extractMax()).toBe(18);
+    expect(maxHeap.values).toEqual([12]);
+    expect(maxHeap.extractMax()).toBe(12);
+    expect(maxHeap.values).toEqual([]);
   })
 
 })
